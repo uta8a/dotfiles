7c280a19554c4a59a260b70207e090d8970b6e1b
Masanori Tani <uta8a@uta8a.org>
2023-02-11 22:31:27 +0000

    feat: remove command This is not dotfile manager component, make my own

---
+++ manager/remove.ts
@@ -0,0 +1,13 @@
+import * as rm from './remove/mod.ts'
+
+const home = Deno.env.get("HOME");
+
+if (!home) throw new Error("$HOME is not set");
+
+await rm.link({ destination: `${home}/.bashrc` });
+await rm.link({ destination: `${home}/.zshrc` });
+await rm.link({ destination: `${home}/.gitconfig` });
+await rm.link({ destination: `${home}/.config/starship.toml` });
+await rm.link({ destination: `${home}/.gnupg/gpg-agent.conf` });
+await rm.link({ destination: `${home}/.sheldon/plugins.toml` });
+await rm.link({ destination: `${home}/.tmux.conf` });
