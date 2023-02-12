c5a92e1bde0028726807da4f060ebb852a81d56b
Masanori Tani <uta8a@uta8a.org>
2023-02-12 03:16:38 +0000

    feat: wip/bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ue
+
+curl -fsSL https://deno.land/x/install/install.sh | sh
+
+export DENO_INSTALL="$HOME/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
+
+# mkdir -p $DIR
+# git clone dotfiles
+# deno task rm
+# deno task deploy
+# deno task check
