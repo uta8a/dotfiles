34fe94c5171cf9b878bae49e4c22cefcae266fa1
Masanori Tani <uta8a@uta8a.org>
2023-12-15 09:13:10 +0000

    add comment to yay setup

--- packages/yay/install.sh
+++ packages/yay/install.sh
@@ -3,3 +3,4 @@
 set -eux -o pipefail
 
 sudo pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si --noconfirm && cd .. && rm -rf yay-bin
+# dotg version --package yay $(yay --version)
--- packages/yay/update.sh
+++ packages/yay/update.sh
@@ -3,4 +3,5 @@
 set -eux -o pipefail
 
 # same with install.sh
+# $PWD is project git root
 bash -c $PWD/packages/yay/install.sh
